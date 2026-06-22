797e7af0f199eb6f3e5046d994d39f5a419fd2c0
DIRGF <112221639+DIRGF@users.noreply.github.com>
2026-06-22 00:23:56 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -17,6 +17,21 @@ const paineis = {
     contabilUnidades: {
         titulo: "Contabilidade Unidades",
         url: "https://app.powerbi.com/view?r=eyJrIjoiNDljNDkyM2YtMTk0OC00NzM3LWJlNTYtODhlZjc2Y2IyOTYxIiwidCI6IjgxYTJmN2VhLTFkM2ItNGI5Ni1iZTYzLTM1MjAzM2FmMGFmZSJ9"
+    },
+
+    processosEdocs: {
+        titulo: "Processos E-Docs",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiMGIwYjdhMzQtM2NmMy00M2QyLTkzMGYtZTY0NGI5MmEzNzBmIiwidCI6ImFlNjRlNjk2LTcyOWUtNDdjYi04OWZlLTcxYTYxYzE4ZGJhNyIsImMiOjF9"
+    },
+
+    valoresServicos: {
+        titulo: "Valores de Serviços e Exames",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiYjJkYTJhYjItODY1ZC00ODRkLWE4MWYtODBlMjBmOWRjNWNlIiwidCI6IjM1YWFmNDRiLWQ2Y2EtNGU1OC1hMmRhLTg5ODZkYzgxZTMzNCJ9"
+    },
+
+    indicadoresHospitalares: {
+        titulo: "Indicadores Hospitalares",
+        url: "https://app.powerbi.com/view?r=eyJrIjoiZTYxNzdlMmUtMjYzNC00NDI3LWIzMzAtZjEwYzFjOGViYTUxIiwidCI6IjM1YWFmNDRiLWQ2Y2EtNGU1OC1hMmRhLTg5ODZkYzgxZTMzNCJ9"
     }
 
 };
@@ -34,18 +49,13 @@ function abrirPainel(chave){
         return;
     }
 
-    document.body.classList.add(
-        "dashboard-mode"
-    );
+    document.body.classList.add("dashboard-mode");
 
-    document.getElementById("home")
-        .style.display = "none";
+    document.getElementById("home").style.display = "none";
 
-    document.getElementById("viewer")
-        .style.display = "block";
+    document.getElementById("viewer").style.display = "block";
 
-    document.getElementById("painelFrame")
-        .src = painel.url;
+    document.getElementById("painelFrame").src = painel.url;
 }
 
 // =========================
@@ -54,18 +64,13 @@ function abrirPainel(chave){
 
 function voltarPortal(){
 
-    document.body.classList.remove(
-        "dashboard-mode"
-    );
+    document.body.classList.remove("dashboard-mode");
 
-    document.getElementById("viewer")
-        .style.display = "none";
+    document.getElementById("viewer").style.display = "none";
 
-    document.getElementById("home")
-        .style.display = "block";
+    document.getElementById("home").style.display = "block";
 
-    document.getElementById("painelFrame")
-        .src = "";
+    document.getElementById("painelFrame").src = "";
 }
 
 // =========================
