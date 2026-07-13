5758a0e7a633e0c68861042c7f48ace11006b3d6
DIRGF <112221639+DIRGF@users.noreply.github.com>
2026-07-13 15:52:35 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ const paineis = {
         cor: "#3498db",
         url:"https://app.powerbi.com/view?r=eyJrIjoiNDljNDkyM2YtMTk0OC00NzM3LWJlNTYtODhlZjc2Y2IyOTYxIiwidCI6IjgxYTJmN2VhLTFkM2ItNGI5Ni1iZTYzLTM1MjAzM2FmMGFmZSJ9"
     },
+ 
 
     contratos:{
         grupo:"Compras e Contratos",
@@ -90,7 +91,7 @@ function gerarMenu(){
             <span>${p.titulo}</span>
         </div>`;
     });
-
+   
     menu.innerHTML+=`
     <div class="menu-title">Gestão</div>
     <div class="menu-item" onclick="abrirTrello()">
@@ -286,6 +287,18 @@ cards.innerHTML+=`
         <strong>Bionexo</strong>
     </div>`;
 
+
+cards.innerHTML+=`
+    <div class="card" onclick="abrirPowerBi()">
+        <div style="margin-bottom:10px;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path fill="#394abc" d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z" />
+            </svg>
+        </div>
+        <strong>Bionexo</strong>
+    </div>`;
+       
             
 }
 
@@ -356,5 +369,9 @@ function abrirDRG(){
 
 function abrirBionexo(){
     window.open("https://bioid-shared.bionexo.com/","_blank");
+}
+
+function abrirPowerBi(){
+    window.open("https://app.powerbi.com/home?tenant=81a2f7ea-1d3b-4b96-be63-352033af0afe","_blank");
 }
 
